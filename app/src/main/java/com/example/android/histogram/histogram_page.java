af2467964d42faabda4d1fa577a7611366de820e
cubman <tolik.abramoff@yandex.ru>
2016-04-17 12:25:22 +0300

    added new window, made it full screen, some event on click

--- app/src/main/java/com/example/android/histogram/histogram_page.java
+++ app/src/main/java/com/example/android/histogram/histogram_page.java
@@ -1,22 +1,22 @@
 package com.example.android.histogram;
 
-import android.content.*;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.*;
 
-
-public class Main extends AppCompatActivity {
+public class histogram_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_histogram_page);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
     }
-    public void onClick(View v) {
-        Intent questionIntent = new Intent(Main.this,
-                histogram_page.class);
-        startActivityForResult(questionIntent, 0);
+
+    public void back_to_menu(View v) {
+        Intent questionIntent = new Intent(histogram_page.this,
+                Main.class);
+        startActivityForResult(questionIntent, 1);
     }
 }
